5f998fc37724572e3969d7f94ba7d586df4d3635
huyvidjk <gs1drakeivy@hotmail.com>
2024-05-09 15:49:23 +0700

    demo

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-function uniquePaths(m, n) {
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = 1;
+function findTheDifference(s, t) {
+  let result = 0;
+  for (const char of s) {
+    result ^= char.charCodeAt(0);
   }
-  for (let j = 0; j < n; j++) {
-    dp[0][j] = 1;
+  for (const char of t) {
+    result ^= char.charCodeAt(0);
   }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-    }
-  }
-  return dp[m - 1][n - 1];
+  return String.fromCharCode(result);
 }
