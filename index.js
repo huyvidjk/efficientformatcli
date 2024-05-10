fe49c098ca5e02db51da3d6dfd734c81273daa8f
huyvidjk <gs1drakeivy@hotmail.com>
2024-05-10 20:47:15 +0700

    demo task

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function findTheDifference(s, t) {
-  let result = 0;
-  for (const char of s) {
-    result ^= char.charCodeAt(0);
+function reverseBetween(head, m, n) {
+  if (!head || m === n) return head;
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let prev = dummy;
+  for (let i = 0; i < m - 1; i++) prev = prev.next;
+  let start = prev.next;
+  let then = start.next;
+  for (let i = 0; i < n - m; i++) {
+    start.next = then.next;
+    then.next = prev.next;
+    prev.next = then;
+    then = start.next;
   }
-  for (const char of t) {
-    result ^= char.charCodeAt(0);
-  }
-  return String.fromCharCode(result);
+  return dummy.next;
 }
